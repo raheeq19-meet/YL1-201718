20a309030b86cdde9e9a30e3a008204aa790cef9
raheeq19-meet <raheeq19@meet.mit.edu>
2017-11-15 18:41:22 +0200

    Lab1=> Lab4 uploaded

---
+++ Lab1/lab1.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.goto(100, 100)
+turtle.goto(0,100)
+turtle.goto(0,0)
+turtle.goto(100,0)
+turtle.goto(100,100)
+
+
+turtle.circle(50)
+turtle.penup()
+turtle.goto(50,20)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.mainloop()
+
+
---
+++ Lab2/lab22.py
@@ -0,0 +1,31 @@
+#def hey(list):
+   # return [list[0],list[-1]]
+#list2=[1,2,3]
+#print(hey(list2))
+
+#list=[1,2,3,4,5,6,7]
+#for value in list:
+ #   if value<5:
+  #      print value
+#new_list=[]
+
+#list=[1,2,3,4,5,6,7]
+#for value in list:
+ #   if value<5:
+  #      new_list.append(value)
+#print(new_list)
+
+a=[1,2,3,4,5,6]
+b=[4,3,6,8,1]
+new_list=[]
+for valuea in a:
+    for valueb in b:
+        if valuea==valueb and :
+            
+
+
+
+
+    
+    
+    
