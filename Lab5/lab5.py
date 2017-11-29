8db5e4ac5bb5d17936bd0aac95526c79a553f3ad
raheeq19-meet <raheeq19@meet.mit.edu>
2017-11-29 18:58:37 +0200

    Lab5

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -1,6 +1,32 @@
-from turtle import Turtle
-class square(Turtle):
-    def __init__(self,size):
+from turtle import *
+#class square(Turtle):
+    #def __init__(self,size):
+   #     Turtle.__init__(self)
+  #      self.shapesize(size)
+ #       self.shape("square")
+#square(5)
+
+class hexagon (Turtle):
+    def __init__(self,size,speed,color):
+        self.speed=speed
+        self.color=color
         Turtle.__init__(self)
-        self.shapesize(size)
-        self.shape("square")
+        self.begin_poly()
+        self.pendown()
+        for i in range (6):
+            self.forward(size)
+            self.left(60)
+        self.end_poly()
+        hexaa=self.get_poly()
+        register_shape("hexagon",hexaa)
+        self.shape("hexagon")
+hexaa=hexagon(100,50,"Red")
+        
+        
+      
+
+
+
+        
+        
+            
